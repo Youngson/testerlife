c5c9ceeed875686e7ebce2a4e25b86dfd2aa7c7c
Youngson <youngson.gu@gmail.com>
2016-11-09 17:34:59 +0800

    * 增加初始化用户角色基础信息 * 测试通过api方式添加用户

--- testerlife/api_1_0/views.py
+++ testerlife/api_1_0/views.py
@@ -14,18 +14,83 @@ from flask_restful import Api, Resource
 from . import api
 from .forms import UserForm,UserAuthForm
 from .. import alembic,db
-from ..model import User,UserAuth
+from ..model import User,UserAuth,Role
 from ..util.result import Result
 
 api = Api(api)
 
 
+@api.resource('/alembic/<action>')
+class DBAlembic(Resource):
+    '''DBAlembic   数据库版本管理
+    Youngson Create at 2016-11-08 15:13
+
+    current():当前版本列表
+    heads():获取没有子版本的版本列表
+    log(start='base', end='heads'):按运行顺序获取版本列表
+    branches():版本分支列表
+    revision(message):创建新版本
+    upgrade(target='heads'):升级
+    downgrade(target=-1):降级
+    merge(revisions='heads'):合并修订
+    stamp(target='heads'):设置当前数据库版本
+    '''
+    def post(self, action=None):
+        if action is not None:
+            parm = '()' if action != 'revision' else '("made changes")'
+            try:
+                result = eval(''.join(['alembic.', action, parm]))
+            except Exception as e:
+                result = {"error":e}
+        current_app.logger.info(result)
+
+        return {"result": str(result)}
+
+@api.resource('/init/<table>')
+class DBInit(Resource):
+    '''DBInit   初始化基础数据
+    Youngson Create at 2016-11-09 11:13
+    '''
+    def __init__(self):
+        '''Constructor for DBInit'''
+        pass
+
+    def get(self):
+        pass
+
+    def post(self,table):
+        if table == 'role':
+            result = Role.init_role()
+        if table == 'user':
+            result = User.init_user()
+        if table == 'user_auth':
+            result = UserAuth.init_user_auth()
+
+        return {"result": result}
+
+    def put(self):
+        pass
+
+    def patch(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def head(self):
+        pass
+
+    def options(self):
+        pass
+
+
 @api.resource('/auth')
 class AuthLogin(Resource):
-    '''用户登录管理'''
-
+    '''AuthLogin   用户登录管理
+    Youngson Create at 2016-11-07 19:13
+    '''
     def post(self):
-        """用户登录"""
+        '''用户登录'''
         user = UserAuth.query.filter_by(identifier=request.form.get('username')).first()
         if user is not None and user.verify_password(request.form.get('password')):
             login_user(user, False)
@@ -39,60 +104,22 @@ class AuthLogin(Resource):
         logout_user()
         return Result.success()
 
-
-@api.resource('/alembic/<action>')
-class DBAlembic(Resource):
-    '''数据库版本管理
-    current():当前版本列表
-    heads():获取没有子版本的版本列表
-    log(start='base', end='heads'):按运行顺序获取版本列表
-    branches():版本分支列表
-    revision(message):创建新版本
-    upgrade(target='heads'):升级
-    downgrade(target=-1):降级
-    merge(revisions='heads'):合并修订
-    stamp(target='heads'):设置当前数据库版本
+@api.resource('/token')
+class AuthToken(Resource):
+    '''AuthToken
+    Youngson Create at 2016-11-09 16:47
     '''
-
-    def get(self, action=None):
-        if action is not None:
-            parm = '()' if action != 'revision' else '("made changes")'
-            result = eval(''.join(['alembic.', action, parm]))
-        current_app.logger.info(result)
-
-        return {"result": str(result)}
-
-
-@api.resource('/user')
-class AdminUser(Resource):
-    """AdminUser管理
-    """
-
     def __init__(self):
-        """Constructor for AdminUser"""
+        '''Constructor for AuthToken'''
         pass
 
+    @login_required
     def get(self):
-        pass
+        token = current_user.generate_auth_token()
+        return {'token': token.decode()}
 
-    # @login_required
     def post(self):
-        uid = request.args.get('uid')
-        user = User.query.get(uid) if uid is not None else User()
-        user_auth = UserAuth.query.filter_by(user_id=uid).all()  # if uid is not None else None
-        form = UserForm(request.form, user)
-        current_app.logger.info(request.form)
-        current_app.logger.info(request.get_json())
-        if form.validate_on_submit():
-            form.populate_obj(user)
-            db.session.add(user)
-            db.session.commit()
-            result = Result.success('用户添加成功')
-            return result,201,redirect(url_for('.user_auth_edit', uid=user.id))
-        else:
-            current_app.logger.info(form.errors)
-            return Result.error(result=form.errors)
-        # return render_template('edit.html', form=form, rs=user_auth)
+        pass
 
     def put(self):
         pass
@@ -101,4 +128,84 @@ class AdminUser(Resource):
         pass
 
     def delete(self):
+        pass
+
+    def head(self):
+        pass
+
+    def options(self):
+        pass
+
+@api.resource('/user')
+class AdminUser(Resource):
+    '''AdminUser    管理
+    Youngson Create at 2016-11-08 23:13
+    '''
+    def __init__(self):
+        """Constructor for AdminUser"""
+        pass
+
+    def get(self):
+        pass
+
+    @login_required
+    def post(self):
+        uid = request.args.get('uid')
+        user = User.query.get(uid) if uid is not None else User()
+        user_auth = UserAuth.query.filter_by(user_id=uid).all()  if uid is not None else None
+        form = UserForm(csrf_enabled=False)#request.form, user
+        csrf_token = form.hidden_tag()
+        current_app.logger.info(form.hidden_tag())
+        current_app.logger.info(request.form)
+        current_app.logger.info(request.get_json())
+        form.csrf_token = csrf_token
+        if form.validate_on_submit():
+            form.populate_obj(user)
+            return Result.success(user.create(),{"uid":user.id})
+        else:
+            return Result.error(result=form.errors)
+
+    def put(self):
+        pass
+
+    def patch(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def head(self):
+        pass
+
+    def options(self):
+        pass
+
+@api.resource('/user-auth')
+class AdminUserAuth(Resource):
+    '''AdminUserAuth    用户登录账号管理
+    Youngson Create at 2016-11-09 16:15
+    '''
+    def __init__(self):
+        '''Constructor for AdminUserAuth'''
+        pass
+
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+    def put(self):
+        pass
+
+    def patch(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def head(self):
+        pass
+
+    def options(self):
         pass
--- testerlife/model.py
+++ testerlife/model.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Youngson
+# @Eamil:  Youngson.Gu@gmail.com
+# @Date:   2016-11-09 12:48
+# @Last Modified by:   Youngson
+# @Last Modified time: 2016-11-09 12:48
+
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer, SignatureExpired,BadSignature
+from flask import current_app
 from flask_login import UserMixin
 from . import db, bcrypt,login
 
@@ -15,13 +25,32 @@ class User(db.Model):
     '''用户基础信息表'''
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     nickname = db.Column(db.String(128))
     avatar = db.Column(db.String(128))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     user_auth = db.relationship('UserAuth', backref='user', lazy='dynamic')
 
     def __repr__(self):
         return '<User %r>' % self.nickname
+
+    @staticmethod
+    def init_user():
+        admin_role_id = Role.query.with_entities(Role.id).filter_by(name='Admin').first() or 1
+        user_list=[('Admin',admin_role_id[0])]
+        for user in user_list:
+            if User.query.filter_by(nickname=user[0]).first() is None:
+                rs = User(nickname=user[0],role_id=user[1])
+                db.session.add(rs)
+        result = db.session.commit()
+        return result
+
+    def create(self):
+        if User.query.filter_by(nickname=self.nickname).first() is None:
+            db.session.add(self)
+            db.session.commit()
+            return '用户添加成功'
+        else:
+            return '此用户名已经使用'
 
 
 class UserAuth(db.Model, UserMixin):
@@ -45,10 +74,40 @@ class UserAuth(db.Model, UserMixin):
             self.credential = _password
 
     def verify_password(self, _password):
+        user = User.verify_auth_token(_password)
+        if user:
+            return True
         if self.identity_type in ('phone', 'email', 'username'):
             return bcrypt.check_password_hash(self.credential, _password)
         else:
             return bool(self.credential == _password)
+
+    @staticmethod
+    def init_user_auth():
+        admin_user_id = User.query.with_entities(User.id).filter_by(nickname='Admin').first() or 1
+        user_auth_list=[(admin_user_id[0],'username','admin','admin')]
+        for user_auth in user_auth_list:
+            if UserAuth.query.filter_by(identifier=user_auth[1]).first() is None:
+                rs = UserAuth(user_id=user_auth[0],identity_type=user_auth[1],identifier=user_auth[2],password=user_auth[3])
+                db.session.add(rs)
+        result = db.session.commit()
+        return result
+
+    def generate_auth_token(self, expiration = 600):
+        s = Serializer(current_app.config['SECRET_KEY'], expires_in = expiration)
+        return s.dumps({ 'id': self.id })
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            return None # valid token, but expired
+        except BadSignature:
+            return None # invalid token
+        user = UserAuth.query.get(data['id'])
+        return user
 
     def __repr__(self):
         return '<User_Auth %r>' % self.identifier
@@ -61,3 +120,22 @@ class Role(db.Model):
     default = db.Column(db.Boolean, default=False, index=True)
     permissions = db.Column(db.Integer)
     users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+    @staticmethod
+    def init_role():
+        role_list = [
+            ('Admin',False,0),
+            ('项目经理', False, 0),
+            ('项目主管', False, 0),
+            ('成员', False, 0),
+            ('Guest', True, 0),
+        ]
+        for role in role_list:
+            if Role.query.filter_by(name=role[0]).first() is None:
+                rs = Role(name=role[0],default=role[1],permissions=role[2])
+                db.session.add(rs)
+        result = db.session.commit()
+        return result
